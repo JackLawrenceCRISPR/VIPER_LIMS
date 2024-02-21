be9505efd9071cf5856f9be3c5de88c1f16f6ec7
JackLawrenceCRISPR <133519528+JackLawrenceCRISPR@users.noreply.github.com>
2024-02-21 14:57:02 +0000

    Added VIPER_Installer sourcecode

---
+++ VIPER_Installer/VIPER_LIMS/__init__.py
@@ -0,0 +1,10 @@
+from VIPER_LIMS.VIPER_Installer import Install
+
+__author__ = "Jack Lawrence"
+__date__ = "2024-02-21"
+
+__all__ = [
+    "Install",
+]
+
+print("To install VIPER use: VIPER_LIMS.Install()")
